eef5b8cc7a089d675cbbb22e0f6eaa4f175090e8
Heccubernny <ipom4eva@gmail.com>
2020-09-28 14:22:43 +0100

    Updating codes

--- polls/admin.py
+++ polls/admin.py
@@ -15,8 +15,11 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date')
-
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # and a list filter
+    list_filter = ['pub_date']
+    # Add a search input box
+    search_fields = ['question_text']
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 
--- polls/models.py
+++ polls/models.py
@@ -20,6 +20,11 @@ class Question(models.Model):
         """
         now = timezone.now()
         return now - datetime.timedelta(days = 1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    # Kind of change the True or False to an icon True or False
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'published recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
